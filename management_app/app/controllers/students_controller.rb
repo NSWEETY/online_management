435160e9da906d4c7961534e755f137bcd23ea0a
NSWEETY <naiksweety2000@gmail.com>
2020-04-09 18:04:35 +0530

    show page

--- management_app/app/controllers/students_controller.rb
+++ management_app/app/controllers/students_controller.rb
@@ -2,13 +2,16 @@ class StudentsController < ApplicationController
   def index
   @students = Student.all
   end
+  def show
+  	@student = Student.find(params[:id])
+  end
   def new
   	@student =Student.new
   end
   def create
   	@student = Student.new(student_params)
   	if @student.save
-  		flash[:success] = "You have successfully signed up"
+  		flash[:notice] = "You have successfully signed up"
   		redirect_to root_path
   	else
   		render 'new'
